e7b06793ec328ea7b98290171fae7a0857393a6f
Amit Kashinath Birajdar <amit1711040@gmail.com>
2026-03-07 00:36:24 +0530

    class name changed from Employee to EmployeeController

--- Module 02- SB MVC & RESTful API's/Module02/src/main/java/com/codingShuttle/Module02/controller/EmployeeController.java
+++ Module 02- SB MVC & RESTful API's/Module02/src/main/java/com/codingShuttle/Module02/controller/EmployeeController.java
@@ -4,7 +4,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-public class Employee {
+public class EmployeeController {
 
     @GetMapping("/employee")
     public String getEmployeeDetails(){
